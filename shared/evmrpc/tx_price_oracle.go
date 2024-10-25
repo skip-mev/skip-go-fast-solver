1ef7ddccbbd5917eed55450eb845c3bc32958c64
Matt Acciai <matt@skip.money>
2024-10-25 16:55:38 -0400

    implmenet tx price oracle in order to get the price to execute a tx on evm chains in uusdc

---
+++ shared/evmrpc/tx_price_oracle.go
@@ -0,0 +1,74 @@
+package evmrpc
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/skip-mev/go-fast-solver/shared/clients/coingecko"
+)
+
+const (
+	coingeckoEthID       = "ethereum"
+	coingeckoUSDCurrency = "usd"
+)
+
+// Oracle is a evm uusdc tx execution price oracle that determines the price of
+// executing a tx on chain in uusdc.
+type Oracle struct {
+	coingecko coingecko.PriceClient
+}
+
+// NewOracle creates a new evm uusdc tx execution price oracle.
+func NewOracle(coingecko coingecko.PriceClient) *Oracle {
+	return &Oracle{coingecko: coingecko}
+}
+
+// TxFeeUUSDC estimates what the cost in uusdc would be to execute a tx. The
+// tx's gas fee cap and gas limit must be set.
+func (o *Oracle) TxFeeUUSDC(ctx context.Context, tx *types.Transaction) (*big.Int, error) {
+	if tx.Type() != types.DynamicFeeTxType {
+		return nil, fmt.Errorf("tx type must be dynamic fee tx, got %d", tx.Type())
+	}
+
+	estimatedPricePerGas := tx.GasFeeCap()
+	if estimatedPricePerGas == nil {
+		return nil, fmt.Errorf("tx's gas fee cap must be set")
+	}
+
+	estimatedGasUsed := tx.Gas()
+	return o.gasCostUUSDC(ctx, estimatedPricePerGas, big.NewInt(int64(estimatedGasUsed)))
+}
+
+// gasCostUUSDC converts an amount of gas and the price per gas in gwei to
+// uusdc based on the current CoinGecko price of ethereum in usd.
+func (o *Oracle) gasCostUUSDC(ctx context.Context, pricePerGasGwei *big.Int, gasUsed *big.Int) (*big.Int, error) {
+	txFeeGwei := new(big.Float).SetInt(new(big.Int).Mul(gasUsed, pricePerGasGwei))
+	fmt.Println("tx fee gwei", txFeeGwei.String())
+
+	// get the price of eth in usd from coin gecko and convert to gwei
+	ethPriceUSD, err := o.coingecko.GetSimplePrice(ctx, coingeckoEthID, coingeckoUSDCurrency)
+	if err != nil {
+		return nil, fmt.Errorf("getting coin gecko price of ethereum in USD: %w", err)
+	}
+	const GWEI_PER_ETH = 1000000000
+	gweiPriceUSD := new(big.Float).Quo(big.NewFloat(ethPriceUSD), big.NewFloat(GWEI_PER_ETH))
+	fmt.Println("gwei price usd", gweiPriceUSD.String())
+
+	// get the tx fee in usd and convert to uusdc
+	txFeeUSD := new(big.Float).Mul(txFeeGwei, gweiPriceUSD)
+	fmt.Println("tx fee usd", txFeeUSD.String())
+
+	// assuming 1usd == 1usdc
+	const UUSDC_PER_USDC = 1000000
+	txFeeUUSDC := new(big.Float).Mul(txFeeUSD, big.NewFloat(UUSDC_PER_USDC))
+	fmt.Println("tx fee uusdc", txFeeUUSDC.String())
+
+	fmt.Println("tx fee uusdc is int", txFeeUUSDC.IsInt())
+
+	// we may be off by 1 uusdc in either direction here due to floating point
+	// numbers being annoying
+	uusdcFee, _ := txFeeUUSDC.Int(nil)
+	return uusdcFee, nil
+}
